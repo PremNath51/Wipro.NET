b03349a825b57d09b1c07117deb2181f3b6f3cd9
PremNath RajaSekaran <rpremnath51@live.com>
2020-04-22 15:48:29 +1000

    remove
    remove

--- RestApiApplication-Task2/RestApi/ClientApp/components/App.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { Http, Headers } from '@angular/http';
-
-@Component({
-    selector: 'app',
-    templateUrl: 'ClientApp/components/App.html'
-})
-export class AppComponent {
-}
